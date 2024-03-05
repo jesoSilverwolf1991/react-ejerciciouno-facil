dd6ab4e67a7a056b6c083bdcd5b17b9a8e4e8a33
jesoSilverwolf1991 <jeso_sar17@hotmail.com>
2024-03-05 16:15:27 -0500

    Ejercicio uno react

---
+++ src/Components/Header.jsx
@@ -0,0 +1,14 @@
+
+
+ export const Header = () => {
+  return (
+    <header className="bg-black">
+      <div className="font-bold grid justify-items-center text-[50px] text-white ">
+        <h1>¡Hola, Mundo!</h1>
+     
+      </div>
+    </header>
+  )
+}
+
+export default Header
